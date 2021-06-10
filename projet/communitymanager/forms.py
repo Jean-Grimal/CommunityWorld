ee5a3dc612ebb8d4e4b38ad2415b48d488af87a6
Jean-Grimal <jeanbgrimal@gmail.com>
2021-06-10 21:45:30 +0200

    F4

---
+++ projet/communitymanager/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Post, Commentaire, priorite
+
+
+class Ecrire_Un_post(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('titre','description','communaute','priorite','evenementiel','date_evenement')
+        #fields = '__all__'
+
+class FormulaireCom(forms.ModelForm):
+    class Meta:
+        model = Commentaire
+        fields = ('contenu',)
+
+class FormulaireFiltrage(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('priorite',)
